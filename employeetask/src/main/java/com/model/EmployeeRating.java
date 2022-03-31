751cae086d7dbfca4fca4ec49804d2d570c2fb68
sahithi1028 <97524762+sahithi1028@users.noreply.github.com>
2022-03-31 09:00:06 +0530

    Create EmployeeRating.java

---
+++ employeetask/src/main/java/com/model/EmployeeRating.java
@@ -0,0 +1,42 @@
+package com.model;
+import javax.persistence.*;
+
+@Entity
+public class EmployeeRating {
+	@Id
+	@GeneratedValue
+	private int employeeRatingId;
+	@OneToOne
+	private Task task;
+	@Enumerated(EnumType.STRING)
+	private Rating rating;
+	
+	public EmployeeRating() {
+		
+	}
+
+	public int getEmployeeRatingId() {
+		return employeeRatingId;
+	}
+
+	public void setEmployeeRatingId(int employeeRatingId) {
+		this.employeeRatingId = employeeRatingId;
+	}
+
+	public Task getTask() {
+		return task;
+	}
+
+	public void setTask(Task task) {
+		this.task = task;
+	}
+
+	public Rating getRating() {
+		return rating;
+	}
+
+	public void setRating(Rating rating) {
+		this.rating = rating;
+	}
+
+}
